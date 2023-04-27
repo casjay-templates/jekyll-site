84d00c61b1d0d2c207aca879d6a7910494260641
casjay <git-admin@casjaysdev.com>
2023-04-27 14:08:05 -0400

    🗃️ Committing everything that changed 🗃️
    assets/js/fetch-url.js
    casjaysdev-jekyll-theme.gemspec
    _config.yml
    Gemfile.lock
    _includes/casjaysdev/sharethis.html
    _includes/casjaysdev/socialfooter.html

--- assets/js/fetch-url.js
+++ assets/js/fetch-url.js
@@ -1,5 +1,5 @@
 fetch(
-  "https://github.com/casjaysdev-sites/static.casjay.net/raw/main/README.md"
+  "https://github.com/casjaysdev-sites/casjaysdev-sites.github.io/static/raw/main/README.md"
 )
   .then(function (response) {
     return response.text();
