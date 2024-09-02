55f4f2996089341f9cfec966fb8eb4a30e668412
casjay <git-admin@casjaysdev.pro>
2024-09-02 19:20:17 -0400

    🗃️ Committing everything that changed 🗃️
    assets/js/errorpages/homepage.js

--- assets/js/errorpages/homepage.js
+++ assets/js/errorpages/homepage.js
@@ -1,13 +1,10 @@
-function loadDomain(getURI, getID, getMessage) {
-  let id = getID;
+function homepage(getURI) {
   let baseURI = getURI;
-  let message = getMessage;
   let port = location.port;
   let url = location.hostname;
   let proto = location.protocol;
   let baseURL = baseURI || location.pathname;
   let base = baseURL.slice(0, baseURL.lastIndexOf('/'));
-  if (!id) id = 'display-domain';
   if (!base) base = '/';
   if (!port)
     if (proto == 'https:') {
